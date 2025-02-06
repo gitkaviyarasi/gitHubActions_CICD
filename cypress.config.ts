d612578f794dfd98a4e06d1bf1434721bc5103dc
gitkaviyarasi <kaviyarasikrishnannj@gmail.com>
2025-02-06 11:45:57 -0500

    changed the baseurl for testing in github actions

--- cypress.config.ts
+++ cypress.config.ts
@@ -12,7 +12,8 @@ export default defineConfig({
   },
 
   e2e: {
-    baseUrl: 'http://localhost:3001',
+    baseUrl: 'https://githubactions-cicd.onrender.com/',
+    // baseUrl: 'http://localhost:3001',
     specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
     setupNodeEvents(on, config) {
       // implement node event listeners here
